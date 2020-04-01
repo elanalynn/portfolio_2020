5b33bc48542441238e2a5a1b7711dbfb57f8f8fd
Elana Kopelevich <ekopelevich@gmail.com>
2020-03-31 18:56:18 -0600

    wire up form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -8,21 +8,24 @@ const contact = () => (
     <form
       className="contact"
       method="post"
+      name="contact"
       netlify-honeypot="bot-field"
       data-netlify="true"
+      data-netlify-recaptcha="true"
     >
       <label>
         Email
-        <input type="email" name="email" />
+        <input type="email" name="email" required />
       </label>
       <label>
         Name
-        <input type="text" name="name" />
+        <input type="text" name="name" required />
       </label>
       <label>
         Message
-        <textarea rows="10" type="text" name="message" />
+        <textarea rows="10" type="text" name="message" required />
       </label>
+      <div data-netlify-recaptcha="true"></div>
       <button type="submit">Send</button>
     </form>
   </Layout>
