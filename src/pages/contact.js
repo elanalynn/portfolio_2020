f3f8b1b730c745ccfd86b34051e4359168fee37d
Elana Kopelevich <ekopelevich@gmail.com>
2020-03-31 19:27:17 -0600

    add social icons

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,13 +5,36 @@ import { Layout } from "../components/Layout/Layout"
 const contact = () => (
   <Layout pageTitle="Contact">
     <h1>Contact</h1>
+    <div className="social-icons">
+      <a href="https://github.com/elanalynn" target="_blank">
+        <img
+          height="32"
+          width="32"
+          src="https://unpkg.com/simple-icons@latest/icons/github.svg"
+        />
+      </a>
+      <a href="https://www.linkedin.com/in/elanalynn/" target="_blank">
+        <img
+          height="32"
+          width="32"
+          src="https://unpkg.com/simple-icons@latest/icons/linkedin.svg"
+        />
+      </a>
+      <a href="https://www.twitter.com/elanalynn/" target="_blank">
+        <img
+          height="32"
+          width="32"
+          src="https://unpkg.com/simple-icons@latest/icons/twitter.svg"
+        />
+      </a>
+    </div>
     <form
       className="contact"
       method="post"
+      action="/success"
       name="contact"
       netlify-honeypot="bot-field"
       data-netlify="true"
-      data-netlify-recaptcha="true"
     >
       <label>
         Email
@@ -23,9 +46,8 @@ const contact = () => (
       </label>
       <label>
         Message
-        <textarea rows="10" type="text" name="message" required />
+        <textarea rows="5" type="text" name="message" required />
       </label>
-      <div data-netlify-recaptcha="true"></div>
       <button type="submit">Send</button>
     </form>
   </Layout>
---
+++ src/pages/success.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+import { Layout } from "../components/Layout/Layout"
+
+const success = () => (
+  <Layout pageTitle="Success">
+    <p>Thanks for reaching out! I'll get back to if you're for real.</p>
+  </Layout>
+)
+
+export default success
