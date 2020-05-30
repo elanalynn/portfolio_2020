407c6542a83c8ff7a9d8478fc7064f1f8ddd2467
Elana Kopelevich <ekopelevich@gmail.com>
2020-05-30 15:30:34 -0600

    mend

--- src/components/MobileMenu/MobileMenu.js
+++ src/components/MobileMenu/MobileMenu.js
@@ -7,10 +7,8 @@ import "./index.scss"
 export const MobileMenu = ({ path }) => {
   const [open, setOpen] = useState(false)
   return (
-    <div className="mobile">
-      <button className="burger" onClick={() => setOpen(!open)}>
-        <Burger />
-      </button>
+    <div className="mobile" onClick={() => setOpen(!open)}>
+      <Burger />
       <Navigation navClass={open ? "open" : "closed"} path={path} />
     </div>
   )
