afd1392b16b96107f16bfb427b0272570d2a8514
Elana Kopelevich <ekopelevich@gmail.com>
2020-06-23 21:46:54 -0600

    clean up posts

--- src/assets/posts/index.js
+++ src/assets/posts/index.js
@@ -1,5 +1,3 @@
-import * as artichoke from "./artichoke.jpg"
-import * as bicycle from "./bicycle.jpg"
 import * as squirrel from "./squirrel.jpg"
 
-export const postImages = [artichoke, bicycle, squirrel]
+export const postImages = [squirrel]
